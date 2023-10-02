42a5b64eeefcf668e4eb3d1b6551d698c7059a6b
ABINESH S <abineshsj288@gmail.com>
2023-10-02 22:18:05 +0530

    Updated

--- Test_Scripts/Run_Scripts.py
+++ Test_Scripts/Run_Scripts.py
@@ -6,7 +6,6 @@ d = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
 path = r"D:\Users\Abinesh\PycharmProjects\SeleniumFrameworks\DemoWebShop\Reports"
 
 run_command = f"pytest -vs --template=html1/index.html --report={path}/Test_Report_{d}.html --disable-warnings"
-
 subprocess.run(run_command, shell=True)
 
 
